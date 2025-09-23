6f40a55b794d2ae9950510c8f136504847339f71
HyeontaeSung <107699360+HyeontaeSung@users.noreply.github.com>
2025-09-23 12:21:44 +0000

    Deploying to gh-pages from @ HyeontaeSung/hyeontaesung.github.io@9fb06ec53f55d3a89530ab5b93356998f0f91512 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HyeontaeSung/HyeontaeSung",
+    "data-repo": "HyeontaeSung/hyeontaesung.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
